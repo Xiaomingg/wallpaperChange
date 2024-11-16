6c57d6cb99c73524113dfcb15e18d4e05cd9e340
James <jameswmartin4@gmail.com>
2024-11-16 15:18:16 +0000

    loading required packages temp🐌

--- changeImage.go
+++ changeImage.go
@@ -3,8 +3,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"unsafe"
 
+	"github.com/joho/godotenv"
 	"pkg.go.dev/golang.org/x/sys/windows"
 )
 
@@ -12,8 +14,13 @@ var user32DLL = windows.NewLazyDLL("user32.dll")
 var procSystemParameterInfo = user32DLL.NewProc("SystemParametersInfoW")
 
 func main() {
-	imgPath, _ := windows.UTF16PtrFromString(`C:\Users\James\Documents\coding\Projects\wallpaperChange\wallpaper1.jpg`)
-	img2Path, _ := windows.UTF16PtrFromString(`C:\Users\James\Documents\coding\Projects\wallpaperChange\wallpaper2.jpg`)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	imgPath, _ := windows.UTF16PtrFromString(`wallpaper1Path`)
+	img2Path, _ := windows.UTF16PtrFromString(`wallpaper2Path`)
 	fmt.Println("Changing...")
 	procSystemParameterInfo.Call(20, 0, uintptr(unsafe.Pointer(imgPath)), 0x001A)
 }
